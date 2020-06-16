88c771a1180cc9ddfb45fe72949dfd9f813049f5
Pjotrs Sakov <sakov.p@gmail.com>
2020-06-16 22:55:43 +0200

    Initial Auth

---
+++ server/auth/index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const router = express.Router()
+const Joi  = require('@hapi/joi')
+
+const db  = require('../db/connections')
+const users = db.get('users')
+users.createIndex('username', {unique: true})
+
+
+
+
+const schema = Joi.object({
+    username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+    password: Joi.string()
+        .pattern(new RegExp("(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}).*$"))
+        .required(),
+})
+
+router.get ('/', (req, res)=> {
+    res.json({
+        message: "This route is Working"
+    })
+})
+
+router.post('/signup', (req, res)=>{
+//Working validation 
+    // const validation = schema.validate(req.body)
+    // let {error} = validation
+    // if (error){
+    //     return res.json(error.message)
+    // }else{
+    //     return res.json({message : "all chill"})
+    // }
+
+    const validation = schema.validate(req.body)
+    let {error} = validation
+    if (error){
+        return res.json(error)
+        
+    }else{
+        users.insert({ username: req.body.username, password: req.body.password })
+        return res.json({message : "all chill"})
+        
+    }
+
+    
+  
+
+    const body = Object.values(req.body)
+    for (let i=0; i<body.length; i++) {
+        console.log(body[i])
+    }
+        
+    
+    
+    
+ 
+    
+})
+
+module.exports = router
---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const volleyball = require('volleyball')
+
+ 
+const app = express()
+ 
+app.use(volleyball)
+app.use(express.json())
+const auth = require('./auth')
+ 
+ 
+ app.get('/', (req,res)=>{ 
+     console.log('bash message')
+     res.json({
+     message: "server is up up up , client message"
+ })
+ })
+
+ app.use('/auth', auth)
+
+ function notFound (req,res,next){
+     res.status(404)
+     const error = new Error('Not Found -' + req.originalUrl)
+     next(error)
+ }
+
+ function errorHandler (err, req,res,next){
+    res.status(res.statusCode || 500)
+    res.json({
+        message: err.message,
+        stack: err.stack
+    })
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
+const port = process.env.PORT || 3000
+
+
+ 
+ app.listen(port, ()=> (console.log('listening on port', port)))
